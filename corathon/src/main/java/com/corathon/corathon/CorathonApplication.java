02cf71fc8be489040849f068e72c90df99645ae5
신정환 <95666408+loginshin@users.noreply.github.com>
2024-09-21 18:44:00 +0900

    spring init (셋팅 전)
    spring 3.3.4
    JDK 17

---
+++ corathon/src/main/java/com/corathon/corathon/CorathonApplication.java
@@ -0,0 +1,13 @@
+package com.corathon.corathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorathonApplication.class, args);
+	}
+
+}
---
+++ corathon/src/test/java/com/corathon/corathon/CorathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corathon.corathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
